16ec8884eb7daa68e6815daeae8a6a68c140de70
Simon Becker <aicendur@gmx.de>
2026-02-21 23:25:01 +0100

    Zeiterfassung vorbereitet

--- lib/global.dart
+++ lib/global.dart
@@ -1,5 +1,3 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
 import 'dart:collection';
 
@@ -9,18 +7,35 @@ const Color lightBG = Color.fromRGBO(122, 201, 67, 1);
 AppBar standardAppBar = AppBar(title: Image.asset('Assets/Logo.png'), backgroundColor: darkBG,);
 
 int userLoggedIn = 0;
-
-int selectedIndex = 0;
+int selectedChild = 0;
+int selectedActivity = 0;
 
 List<String> childList = <String>[''];
 List<String> activityList = <String>[''];
 
-List<(TimeOfDay, TimeOfDay, int, int, String)> activityOnDayList = <(TimeOfDay, TimeOfDay, int, int, String)>[(TimeOfDay(hour: 10, minute: 0), TimeOfDay(hour: 11, minute: 0), 0, 0, 'Debug')];
+List<(TimeOfDay, TimeOfDay, int, int, String)> activityOnDayList = <(TimeOfDay, TimeOfDay, int, int, String)>[(TimeOfDay(hour: 10, minute: 0), TimeOfDay(hour: 11, minute: 0), 0, 0, 'Initialwert wird von der DB dann direkt befüllt.')];
 
 
 DropdownMenu<String> dropDownChildren = DropdownMenu<String>(
-                        dropdownMenuEntries: UnmodifiableListView<DropdownMenuEntry<String>>(
-                          childList.map<DropdownMenuEntry<String>>((String name) => DropdownMenuEntry<String>(value: name, label: name, enabled: false))),
-                          enabled : childList.length > 1,
-                          initialSelection: childList.first,
-                      );
+  dropdownMenuEntries: UnmodifiableListView<DropdownMenuEntry<String>>(
+      childList.map<DropdownMenuEntry<String>>((String name) => DropdownMenuEntry<String>(value: name, label: name, enabled: false))),
+    enabled : childList.length > 1,
+    initialSelection: childList.first,
+    onSelected: (value) {
+      if(value != null){
+        selectedChild = childList.indexOf(value);
+      }
+    },
+);
+
+DropdownMenu<String> dropDownActivity = DropdownMenu<String>(
+  dropdownMenuEntries: UnmodifiableListView<DropdownMenuEntry<String>>(
+    activityList.map<DropdownMenuEntry<String>>((String name) => DropdownMenuEntry<String>(value: name, label: name))),
+    enabled : activityList.length > 1,
+    initialSelection: activityList.first,
+    onSelected: (value) {
+      if(value != null){
+        selectedActivity = activityList.indexOf(value);
+      }
+    },
+);
--- lib/pages/employees/timetracker.dart
+++ lib/pages/employees/timetracker.dart
@@ -1,43 +1,109 @@
-import 'package:collection/collection.dart';
+import 'dart:collection';
+
 import 'package:flutter/material.dart';
 import 'package:interval_time_picker/models/visible_step.dart';
 import 'package:management_triangel/global.dart';
 import 'package:interval_time_picker/interval_time_picker.dart';
 
+extension TimeOfDayExtension on TimeOfDay {
+  // Ported from org.threeten.bp;
+  TimeOfDay plusMinutes(int minutes) {
+    if (minutes == 0) {
+      return this;
+    } else {
+      int mofd = hour * 60 + minute;
+      int newMofd = ((minutes % 1440) + mofd + 1440) % 1440;
+      if (mofd == newMofd) {
+        return this;
+      } else {
+        int newHour = newMofd ~/ 60;
+        int newMinute = newMofd % 60;
+        return TimeOfDay(hour: newHour, minute: newMinute);
+      }
+    }
+  }
+}
 
+class TimetrackerScreen extends StatefulWidget {
+  const TimetrackerScreen({super.key});
 
-class TimetrackerScreen extends StatelessWidget {
-  TimetrackerScreen({super.key});
+  @override
+  State<StatefulWidget> createState() => _TimeTrackerState();
+}
+
+class _TimeTrackerState extends State<TimetrackerScreen>{
+
 
   final _startTimeController = TextEditingController();
   final _endTimeController = TextEditingController();
   final _descriptionController = TextEditingController();
 
+  TimeOfDay startTime = TimeOfDay(hour:TimeOfDay.now().hour, minute: TimeOfDay.now().minute - (TimeOfDay.now().minute % 5));
+  TimeOfDay endTime = TimeOfDay(hour:TimeOfDay.now().plusMinutes(30).hour, minute: TimeOfDay.now().plusMinutes(30).minute - (TimeOfDay.now().minute % 5));
+
 
   void _changedDate(DateTime value){
-    switch (value.day % 3) {
-      case 0:
-        activityOnDayList = [];
-        break;
-      case 1:
-        activityOnDayList = [(TimeOfDay(hour: 10, minute: 0), TimeOfDay(hour: 11, minute: 0), 0, 0, 'Was getan')];
-        break;
-      case 2:
-        activityOnDayList = [(TimeOfDay(hour: 10, minute: 0), TimeOfDay(hour: 11, minute: 0), 0, 0, 'Was getan'),(TimeOfDay(hour: 11, minute: 0), TimeOfDay(hour: 12, minute: 30), 1, 0, 'Noch was getan')];
-        break;
-      default:
-    }
+    setState(() {
+      switch (value.day % 3) {
+        case 0:
+          activityOnDayList = [];
+          break;
+        case 1:
+          activityOnDayList = [(TimeOfDay(hour: 10, minute: 0), TimeOfDay(hour: 11, minute: 0), 0, 0, 'Was getan')];
+          break;
+        case 2:
+          activityOnDayList = [(TimeOfDay(hour: 10, minute: 0), TimeOfDay(hour: 11, minute: 0), 0, 0, 'Was getan'),(TimeOfDay(hour: 11, minute: 0), TimeOfDay(hour: 12, minute: 30), 1, 0, 'Noch was getan')];
+          break;
+        default:
+      }
+    });
   }
 
   void _changedactivity(int changedIndex){
-    _descriptionController.text = "blub";
+
+    startTime = activityOnDayList[changedIndex].$1;
+    endTime = activityOnDayList[changedIndex].$2;
+    selectedActivity = activityOnDayList[changedIndex].$3;
+    selectedChild = activityOnDayList[changedIndex].$4;
+    _descriptionController.text = activityOnDayList[changedIndex].$5;
+
+
+    setState(() {
+      
+      dropDownActivity = DropdownMenu<String>(
+        dropdownMenuEntries: UnmodifiableListView<DropdownMenuEntry<String>>(
+          activityList.map<DropdownMenuEntry<String>>((String name) => DropdownMenuEntry<String>(value: name, label: name))),
+          enabled : activityList.length > 1,
+          initialSelection: activityList[activityOnDayList[changedIndex].$3],
+          onSelected: (value) {
+            if(value != null){
+              selectedActivity = activityList.indexOf(value);
+            }
+          },
+      );
+
+      dropDownChildren = DropdownMenu<String>(
+        dropdownMenuEntries: UnmodifiableListView<DropdownMenuEntry<String>>(
+            childList.map<DropdownMenuEntry<String>>((String name) => DropdownMenuEntry<String>(value: name, label: name, enabled: false))),
+          enabled : childList.length > 1,
+          initialSelection: childList[activityOnDayList[changedIndex].$4],
+          onSelected: (value) {
+            if(value != null){
+              selectedChild = childList.indexOf(value);
+            }
+          },
+      );
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    _startTimeController.text = TimeOfDay.now().hour.toString() + ":" + (TimeOfDay.now().minute - (TimeOfDay.now().minute % 5)).toString().padLeft(2, '0');
-    _endTimeController.text = TimeOfDay.now().hour.toString() + ":" + (TimeOfDay.now().minute - (TimeOfDay.now().minute % 5) + 30).toString().padLeft(2, '0');
+    _startTimeController.text = startTime.format(context);
+    _endTimeController.text = endTime.format(context);
 
+    
+
+    int selectedIndex = 0;
 
     return Scaffold(
       appBar: standardAppBar,
@@ -76,12 +142,7 @@ class TimetrackerScreen extends StatelessWidget {
                         ),
                         Expanded(
                           flex : 3,
-                          child : DropdownMenu<String>(
-                              dropdownMenuEntries: UnmodifiableListView<DropdownMenuEntry<String>>(
-                                activityList.map<DropdownMenuEntry<String>>((String name) => DropdownMenuEntry<String>(value: name, label: name))),
-                                enabled : activityList.length > 1,
-                                initialSelection: activityList.first,
-                            )
+                          child : dropDownActivity
                         ),
                         Expanded(
                           flex: 8,
@@ -120,68 +181,75 @@ class TimetrackerScreen extends StatelessWidget {
                             children: [
                               Expanded(
                                 flex : 1,
-                                child : Column(
-                                  children : [
-                                    Expanded(
-                                      flex: 1,
-                                      child : FilledButton(onPressed: () async {
-                                          TimeOfDay? selectedTime = await(showIntervalTimePicker(context: context,
-                                              initialTime: TimeOfDay(hour: TimeOfDay.now().hour, minute: TimeOfDay.now().minute - (TimeOfDay.now().minute % 5)),
-                                              interval: 5,
-                                              visibleStep: VisibleStep.fifths
-                                            )
-                                          );
-                                          if(selectedTime != null && selectedTime.toString().length > 1){
-                                            _startTimeController.text = selectedTime.hour.toString() + ":" + selectedTime.minute.toString().padLeft(2, '0');
-                                          }
-                                        },
-                                        child: TextField(controller: _startTimeController, enabled: false, textAlign: TextAlign.center, textAlignVertical: TextAlignVertical.center, style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold,),)
+                                child: Padding(padding: EdgeInsetsGeometry.all(16),
+                                  child : Column(
+                                    children : [
+                                      Expanded(
+                                        flex: 1,
+                                        child : FilledButton(onPressed: () async {
+                                            TimeOfDay? selectedTime = await(showIntervalTimePicker(context: context,
+                                                initialTime: startTime,
+                                                interval: 5,
+                                                visibleStep: VisibleStep.fifths
+                                              )
+                                            );
+                                            if(selectedTime != null && selectedTime.toString().length > 1){
+                                              startTime = selectedTime;
+                                            }
+                                          },
+                                          child: TextField(controller: _startTimeController, enabled: false, textAlign: TextAlign.center, textAlignVertical: TextAlignVertical.center, style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold,),)
+                                        ),
                                       ),
-                                    ),
-                                    Expanded(
-                                      flex : 1,
-                                      child : Text("Tätigkeitsbeginn")
-                                    ),
-                                    Expanded(
-                                      flex: 1,
-                                      child : FilledButton(onPressed: () async {
-                                              TimeOfDay? selectedTime = await(showIntervalTimePicker(context: context, initialTime: TimeOfDay(hour: TimeOfDay.now().hour, minute: TimeOfDay.now().minute - (TimeOfDay.now().minute % 5) + 30),
-                                              interval: 5,
-                                              visibleStep: VisibleStep.fifths
-                                            )
-                                          );
-                                          if(selectedTime != null && selectedTime.toString().length > 1){
-                                            _endTimeController.text = selectedTime.hour.toString() + ":" + selectedTime.minute.toString().padLeft(2, '0');
-                                          }
-                                        },
-                                        child: TextField(controller: _endTimeController, enabled: false, textAlign: TextAlign.center, textAlignVertical: TextAlignVertical.center, style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),)
+                                      Expanded(
+                                        flex : 1,
+                                        child : Text("Tätigkeitsbeginn")
                                       ),
-                                    ),
-                                    Expanded(
-                                      flex : 1,
-                                      child : Text("Tätigkeitsende")
-                                    ),
-                                    Spacer(flex: 8,)
-                                  ]
+                                      Expanded(
+                                        flex: 1,
+                                        child : FilledButton(onPressed: () async {
+                                                TimeOfDay? selectedTime = await(showIntervalTimePicker(context: context,
+                                                initialTime: endTime,
+                                                interval: 5,
+                                                visibleStep: VisibleStep.fifths
+                                              )
+                                            );
+                                            if(selectedTime != null && selectedTime.toString().length > 1){
+                                              setState(() {
+                                                endTime = selectedTime;
+                                              });
+                                            }
+                                          },
+                                          child: TextField(controller: _endTimeController, enabled: false, textAlign: TextAlign.center, textAlignVertical: TextAlignVertical.center, style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),)
+                                        ),
+                                      ),
+                                      Expanded(
+                                        flex : 1,
+                                        child : Text("Tätigkeitsende")
+                                      ),
+                                      Spacer(flex: 8,)
+                                    ]
+                                  )
                                 )
                               ),
                               Expanded(
                                 flex : 1,
-                                child : Container(
-                                  decoration: BoxDecoration(color : Colors.white),
-                                  child: ListView.builder(
-                                    itemCount: activityOnDayList.length,
-                                    itemBuilder: (context, index) {
-                                      return ListTile(
-                                        title : Text(activityOnDayList[index].$1.format(context) + "-" + activityOnDayList[index].$2.format(context) + " - " + activityList[activityOnDayList[index].$3]),
-                                        tileColor: selectedIndex == index ? Colors.blue : null,
-                                        onTap: () {
-                                            selectedIndex = index;
-                                            _changedactivity(index);
-                                        }
-                                      );
-                                    }
-                                  ),
+                                child: Padding(padding: EdgeInsetsGeometry.all(16),
+                                  child : Container(
+                                    decoration: BoxDecoration(color : Colors.white),
+                                    child: ListView.builder(
+                                      itemCount: activityOnDayList.length,
+                                      itemBuilder: (context, index) {
+                                        return ListTile(
+                                          title : Text(activityOnDayList[index].$1.format(context) + "-" + activityOnDayList[index].$2.format(context) + " - " + activityList[activityOnDayList[index].$3]),
+                                          tileColor: selectedIndex == index ? Colors.blue : null,
+                                          onTap: () {
+                                              selectedIndex = index;
+                                              _changedactivity(index);
+                                          }
+                                        );
+                                      }
+                                    ),
+                                  )
                                 )
                               )
                             ],
@@ -193,12 +261,48 @@ class TimetrackerScreen extends StatelessWidget {
                 ]
               )
             ),
-            Spacer(
-              flex: 2
+            Expanded(
+              flex: 2,
+              child: Row(
+                children: [
+                  Spacer(
+                    flex: 8
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: FilledButton(
+                      onPressed: () {
+                        if(activityOnDayList.length >= selectedIndex){
+                          setState(
+                            (){
+                                activityOnDayList.removeAt(selectedIndex);
+                              }
+                          );
+                        }
+                      },
+                      child: Text('Löschen')),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: FilledButton(
+                      onPressed: () {
+                        setState(
+                          (){
+                            activityOnDayList.add((startTime, endTime, selectedActivity, selectedChild, _descriptionController.text));
+                          }
+                        );
+                      },
+                      child: Text('Speichern')),
+                  )
+                ]
+              ),
             )
           ],
         )
       )
     );
   }
+
 }
+
+//activityDropDown
